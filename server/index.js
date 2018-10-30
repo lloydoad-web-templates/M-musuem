ab5ae5e4ad8ae4f866405897d6e753a5a8341654
Lloyd Dapaah <lloyd.dapaah@yahoo.com>
2018-10-30 16:15:55 -0400

    wrap up

--- server/index.js
+++ server/index.js
@@ -11,3 +11,11 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
     res.send("Hello World");
 })
+
+// port can be any number apparently
+const port = process.env.PORT || 5000;
+
+// a function for listening to app
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
